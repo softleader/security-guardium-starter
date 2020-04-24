087e42c38bfc7a4286d34c447f886817dae62fdb
robert <robert.wang@softleader.com.tw>
2020-04-24 12:09:24 +0800

    add oracle(from dual) AppEvent

--- src/main/java/tw/com/softleader/boot/autoconfigure/security/guardium/SecurityGuardiumAutoConfiguration.java
+++ src/main/java/tw/com/softleader/boot/autoconfigure/security/guardium/SecurityGuardiumAutoConfiguration.java
@@ -1,5 +1,10 @@
 package tw.com.softleader.boot.autoconfigure.security.guardium;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ListableBeanFactory;
@@ -14,13 +19,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import tw.com.softleader.boot.security.guardium.EventDataSupplier;
 import tw.com.softleader.boot.security.guardium.GuardAppEvent;
-import tw.com.softleader.boot.security.guardium.SafeguardAspect;
 import tw.com.softleader.boot.security.guardium.IBMSecurityGuardium10GuardAppEvent;
+import tw.com.softleader.boot.security.guardium.IBMSecurityGuardium10OracleGuardAppEvent;
+import tw.com.softleader.boot.security.guardium.SafeguardAspect;
 import tw.com.softleader.util.StringUtils;
-
-import javax.sql.DataSource;
-import java.util.Collections;
-import java.util.HashMap;
 
 /** @author matt */
 @Configuration
@@ -44,7 +46,7 @@ public class SecurityGuardiumAutoConfiguration {
       name = "softleader.security.guardium.version",
       havingValue = SecurityGuardiumProperties.VERSION_10,
       matchIfMissing = true)
-  public GuardAppEvent guardAppEvent(@Autowired EventDataSupplier supplier) {
+  public GuardAppEvent guardAppEvent(@Autowired EventDataSupplier supplier){
     String dataSourceRef = properties.getDataSourceRef();
     if (StringUtils.isBlank(dataSourceRef)) {
       throw new IllegalArgumentException("dataSourceRef is required");
@@ -60,7 +62,17 @@ public class SecurityGuardiumAutoConfiguration {
       log.info("setting sql statement query timeout for {} seconds", timeout);
       template.setQueryTimeout(timeout);
     }
-    return new IBMSecurityGuardium10GuardAppEvent(template, supplier);
+
+    try (Connection connection = ds.getConnection()) {
+      if (connection.getMetaData().getDatabaseProductName().equalsIgnoreCase("Oracle")) {
+        return new IBMSecurityGuardium10OracleGuardAppEvent(template, supplier);
+      } else {
+        return new IBMSecurityGuardium10GuardAppEvent(template, supplier);
+      }
+    } catch (SQLException e) {
+      log.error("GuardAppEvent getting connection fail");
+      return new IBMSecurityGuardium10GuardAppEvent(template, supplier);
+    }
   }
 
   @Bean
---
+++ src/main/java/tw/com/softleader/boot/security/guardium/IBMSecurityGuardium10OracleGuardAppEvent.java
@@ -0,0 +1,46 @@
+package tw.com.softleader.boot.security.guardium;
+
+import java.lang.reflect.Method;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcOperations;
+
+/**
+ * IBM Security Guardium 10 Oracle的實作，多補上from dual
+ *
+ * @author Robert
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class IBMSecurityGuardium10OracleGuardAppEvent implements GuardAppEvent {
+
+  private static final String EVENT_START = "GuardAppEvent:Start";
+  private static final String EVENT_RELEASED = "GuardAppEvent:Released";
+
+  @NonNull private final JdbcOperations jdbcOperations;
+  @NonNull private final EventDataSupplier eventDataSupplier;
+
+  @Override
+  public void start(Method method, Object[] args) {
+    try {
+      String sql =
+          String.format("SELECT '%s',%s from dual", EVENT_START, eventDataSupplier.collect(method, args));
+      log.trace("Setting an application event: {}", sql);
+      jdbcOperations.execute(sql);
+    } catch (Exception e) {
+      log.error(EVENT_START, e);
+    }
+  }
+
+  @Override
+  public void released() {
+    try {
+      String sql = String.format("SELECT '%s' from dual", EVENT_RELEASED);
+      log.trace("Clearing an application event: {}", sql);
+      jdbcOperations.execute(sql);
+    } catch (Exception e) {
+      log.error(EVENT_RELEASED, e);
+    }
+  }
+}
